8c1ad59346e326a29ff765a87ab2ffeb87ad2c10
Nayanp35 <prajapatinayan35@gmail.com>
2023-01-17 21:10:32 +0000

    started coding

---
+++ index.js
@@ -0,0 +1,70 @@
+let userArray = [];
+function button0() {
+    userArray.push('0');
+    
+}
+
+function button1() {
+    userArray.push('1');
+}
+
+function button2() {
+    userArray.push('2');
+}
+
+function button3() {
+    userArray.push('3');
+}
+
+function button4() {
+    userArray.push('4');
+}
+
+function button5() {
+    userArray.push('5');
+}
+
+function button6() {
+    userArray.push('6');
+}
+
+function button7() {
+    userArray.push('7');
+}
+
+function button8() {
+    userArray.push('8');
+}
+
+function button9() {
+    userArray.push('9');
+}
+
+function buttonDot() {
+    userArray.push('.');
+}
+
+function buttonPlus() {
+    userArray.push('+');
+}
+
+function buttonMinus() {
+    userArray.push('-');
+}
+
+function buttonMulti() {
+    userArray.push('x');
+}
+
+function buttonDivision() {
+    userArray.push('÷');
+}
+
+function buttonEqual() {
+    userArray.push('=');
+}
+
+function OneBigFunction() {
+    
+}
+
