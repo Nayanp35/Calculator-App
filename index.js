b05796b03d1a6a9236a00ebf3cee93ba1848ac3f
Nayanp35 <prajapatinayan35@gmail.com>
2023-01-18 14:28:55 +0000

    trying to push

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let userArray = [];
+
 function button0() {
     userArray.push('0');
-    
 }
 
 function button1() {
@@ -60,11 +60,60 @@ function buttonDivision() {
     userArray.push('÷');
 }
 
-function buttonEqual() {
-    userArray.push('=');
-}
+// function concatenate(array) {
+//     const string = array.join('');
+//     let concatedArr = [];
+//     let digitIndex;
+//     let operatorIndex;
+//     for (let i = 0; i < string.length; i++) {
+//         if (/\d/.test(string[i]) && !(/\d/.test(string[i - 1]))) digitIndex = i;
+//         if (!(/\d/.test(string[i]))) {
+//             operatorIndex = i;
+//             concatedArr.push(string.substring(digitIndex, operatorIndex));
+//             concatedArr.push(string[i]);
+//         }
+//     }
+//     return concatedArr;
+// }
 
-function OneBigFunction() {
-    
+// let concatenatedArray = concatenate(userArray)
+
+// function turnToNumber(arr) {
+//     return arr.map(num => {
+//         if (/\d/.test(num)) {
+//             return Number(num)
+//         }
+//         else {
+//             return num;
+//         }
+//     });
+// }
+
+// let turnToNumberArray = turnToNumber(concatenatedArray)
+
+// function calculate(arr) {
+//     let result = arr[0];
+//     for (let i = 1; i < arr.length; i += 2) {
+//         if (arr[i] === '+') {
+//             result += arr[i + 1];
+//         }
+//         else if (arr[i] === '-') {
+//             result -= arr[i + 1];
+//         }
+//         else if (arr[i] === 'x') {
+//             result *= arr[i + 1];
+//         }
+//         else if (arr[i] === '÷') {
+//             result /= arr[i + 1];
+//         }
+//     }
+//     return result;
+// }
+
+// let result = calculate(turnToNumberArray);
+
+function buttonEqual() {
+    let join = userArray.join("");
+    console.log(eval(join))
 }
 
