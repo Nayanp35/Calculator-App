871d02a5b66dc6b092dbbbd3ee0d24231ab5cfc8
Nayanp35 <prajapatinayan35@gmail.com>
2023-02-09 18:59:18 +0000

    made improvements

--- index.js
+++ index.js
@@ -1,96 +1,39 @@
 let userArray = [];
 
-function button0() {
-    userArray.push('0');
-    document.getElementById("display").innerHTML += 0;
-}
-
-function button1() {
-    userArray.push('1');
-    document.getElementById("display").innerHTML += 1;
-}
-
-function button2() {
-    userArray.push('2');
-    document.getElementById("display").innerHTML += 2;
-}
-
-function button3() {
-    userArray.push('3');
-    document.getElementById("display").innerHTML += 3;
-}
-
-function button4() {
-    userArray.push('4');
-    document.getElementById("display").innerHTML += 4;
-}
-
-function button5() {
-    userArray.push('5');
-    document.getElementById("display").innerHTML += 5
-}
-
-function button6() {
-    userArray.push('6');
-    document.getElementById("display").innerHTML += 6
-}
-
-function button7() {
-    userArray.push('7');
-    document.getElementById("display").innerHTML += 7;
-}
-
-function button8() {
-    userArray.push('8');
-    document.getElementById("display").innerHTML += 8;
-}
-
-function button9() {
-    userArray.push('9');
-    document.getElementById("display").innerHTML += 9;
-}
-
-function buttonDot() {
-    userArray.push('.');
-    document.getElementById("display").innerHTML = '.';
-}
-
-function buttonPlus() {
-    userArray.push('+');
-    document.getElementById("display").innerHTML = '';
-}
-
-function buttonMinus() {
-    userArray.push('-');
-    document.getElementById("display").innerHTML = '';
-}
-
-function buttonMulti() {
-    userArray.push('*');
-    document.getElementById("display").innerHTML = '';
-}
-
-function buttonDivision() {
-    userArray.push('/');
-    document.getElementById("display").innerHTML = '';
+function numberButton(num) {
+    userArray.push(num);
+    document.getElementById("display").innerHTML += num;
 }
 
 function buttonBackspace() {
     userArray.pop();
-    document.getElementById("display").innerHTML = '';
+    document.getElementById("display").innerHTML = userArray.join("");
 }
 
 function buttonAC() {
     userArray = [];
-    document.getElementById("display").innerHTML = 0;
+    document.getElementById("display").innerHTML = '';
 }
 
 function buttonPercentage() {
     let join = userArray.join("")
     let num = Number(join)
-    console.log(num / 100)
+    let result = num / 100;
+    userArray = [];
+    userArray.push(result)
     document.getElementById("display").innerHTML = num / 100;
 }
+
+
+function buttonEqual() {
+    let join = userArray.join("");
+    let result = eval(join)
+    userArray = [];
+    userArray.push(result);
+    document.getElementById("display").innerHTML = eval(join);
+}
+
+
 // function concatenate(array) {
 //     const string = array.join('');
 //     let concatedArr = [];
@@ -143,10 +86,5 @@ function buttonPercentage() {
 
 // let result = calculate(turnToNumberArray);
 
-function buttonEqual() {
-    let join = userArray.join("");
-    console.log(eval(join))
-    document.getElementById("display").innerHTML = eval(join);
-}
 
 
